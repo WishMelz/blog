35f6851ce6fde7de3d1e6e3ab94b6a1a94548a03
Your Name <you@example.com>
2019-11-07 17:54:41 +0800

    add:登录遮罩层

--- web/db.js
+++ web/db.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql')
 const { db } = require('./config')
     // 配置数据库账号、密码
-const connection = mysql.createConnection(db)
+    // const connection = mysql.createConnection(db)
+const connection = mysql.createPool(db)
     // 连接数据库
-connection.connect(function(err) {
-        if (err) {
-            console.log("出错了");
+    // connection.connect(function(err) {
+    //         if (err) {
+    //             console.log("出错了");
 
-            connection.connect()
-        }
-    })
-    // 导出 connection
+// connection.connect()
+//         }
+//     })
+// 导出 connection
 module.exports = connection
